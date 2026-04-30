4387e6795d4a40038042618aa3c4d7d7f3cf82d8
aryooo11 <machfudzaryo@gmail.com>
2026-04-30 09:07:18 +0700

    setup fe bookings

---
+++ resources/js/Pages/Organizer/Bookings.jsx
@@ -0,0 +1,12 @@
+import DashboardLayout from '@/Layouts/DashboardLayout'
+import { Head } from '@inertiajs/react'
+import React from 'react'
+
+export default function Bookings() {
+  return (
+    <DashboardLayout>
+            <Head title="Dashboard" />
+    <div>Bookings</div>
+    </DashboardLayout>
+  )
+}
